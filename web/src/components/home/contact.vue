664252e77022b5ec1dca834261824c42e3dd77c3
Andrey Romancev <andrey@romancev.com>
2017-08-15 21:11:01 +0300

    Add validation and sent status.

--- web/src/components/home/contact.vue
+++ web/src/components/home/contact.vue
@@ -2,15 +2,27 @@
     <transition name="modal">
         <div class="modal__mask" @click="$emit('close')">
             <div class="modal__wrapper">
-                <table class="modal__container" @click.stop>
+                <table class="modal__container" @click="preventClose">
                     <tr><td>
-                        <input v-model="email" class="field email" type="email" placeholder="Email"/>
+                        <input v-model="email"
+                               v-bind:disabled="isLocked"
+                               v-bind:class="{ error: isEmailError }"
+                               v-on:input="isEmailError = false"
+                               v-on:change="isEmailError = false"
+                               class="field email" type="email" placeholder="Email"/>
                     </td></tr>
                     <tr><td>
-                        <textarea v-model="message" class="field message" placeholder="Message"></textarea>
+                        <textarea v-model="message"
+                                  v-bind:disabled="isLocked"
+                                  class="field message" placeholder="Message"></textarea>
                     </td></tr>
                     <tr><td>
-                        <div class="modal__button" @click="send">send</div>
+                        <div class="modal__button" @click="send">
+                            <div v-bind:class="{ active: isSent }" class="modal__button__blinder">
+                                <div class="modal__button__message">sent</div>
+                            </div>
+                            send
+                        </div>
                     </td></tr>
                 </table>
             </div>
@@ -24,19 +36,37 @@ import * as axios from 'axios'
 export default {
     data () {
         return {
-            showModal: false,
+            isEmailError: false,
+            isLocked: false,
+            isSent: false,
             email: '',
             message: ''
         }
     },
 
     methods: {
-        send: function () {
+        send: function (e) {
+            if (this.isSent) {
+                return
+            }
+            e.isSendEvent = true
+            if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.email)) {
+                this.isEmailError = true
+                return
+            }
+            this.isEmailError = false
+            this.isLocked = true
             axios.post('/api/contact', {
                 email: this.email,
                 message: this.message
             })
-            this.$emit('close')
+            this.isSent = true
+        },
+
+        preventClose: function (e) {
+            if (!this.isSent || e.isSendEvent) {
+                e.stopPropagation()
+            }
         }
     }
 }
@@ -66,7 +96,7 @@ export default {
     max-width: 400px
     margin: 0 auto
     padding: 20px 30px 10px 30px
-    background-color: #fff
+    background-color: white
     color: $c-background
     border-radius: 1px
     box-shadow: 0 2px 8px rgba(0, 0, 0, .33)
@@ -105,10 +135,18 @@ export default {
     &:-moz-placeholder
       color: $c-text
 
+  .email
+    border: 2px solid transparent
+    transition: border .2s
+    &.error
+      border: 2px solid #c70000
+
   .message
     height: 150px
 
   .modal__button
+    display: block
+    position: relative
     text-align: center
     font-family: 'Montserrat'
     cursor: pointer
@@ -118,6 +156,22 @@ export default {
     &:hover
       background: $c-background
       color: $c-text
+
+  .modal__button__blinder
+    position: absolute
+    width: 100%
+    top: 0
+    left: 0
+    height: 0
+    background: white
+    color: $c-background
+    overflow: hidden
+    transition: height .2s
+    cursor: default
+    &.active
+      height: 100%
+  .modal__button__message
+    padding: 10px
 
 </style>
 
